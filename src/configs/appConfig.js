cd0854a39b0f4f3ea7db7b898d4eaf307158bd74
= <=>
2020-11-08 19:58:20 +0300

    modified for Taaooma

--- src/configs/appConfig.js
+++ src/configs/appConfig.js
@@ -1,4 +1,4 @@
 export default {
   background: '#e3f2fd',
-  siteName: 'Manz Studios',
+  siteName: 'Taaooma',
 }
--- src/configs/socialLinks.js
+++ src/configs/socialLinks.js
@@ -1,7 +1,7 @@
 export default [
   {
     id: 'person-page',
-    url: 'https://www.amanze.me',
+    url: 'https://www.taaooma.com',
     title: 'Personal Website',
     iconUrl: 'https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light',
     subTitle: 'This is personal portfolio website page',
@@ -13,8 +13,8 @@ export default [
   },
   {
     id: 'yt-page',
-    url: 'https://www.youtube.com/channel/UCa475eqLbfWj2wRbdhQU6fA',
-    title: 'Amanze Ogbonna Youtube Channel Page',
+    url: 'https://www.youtube.com/channel/UCvG_RH3tAekGN5rPUVUmcIg',
+    title: 'Taaooma Youtube Channel Page',
     icon: 'fab fa-youtube',
     subTitle: 'This is my youtube channel',
     colors: {
@@ -24,8 +24,8 @@ export default [
   },
   {
     id: 'fb-page',
-    url: 'https://www.facebook.com/africoder',
-    title: 'Africoder Facebook',
+    url: 'https://www.facebook.com/taaooma',
+    title: 'Taaooma Facebook',
     icon: 'fab fa-facebook',
     subTitle: 'This is my Facebook Page',
     colors: {
